fd89df2a816e7d918a4ce727528b909bdd2e8d69
muthu-kc <muthu.phpian@gmail.com>
2017-11-17 12:24:14 +0530

    confi

--- config.php
+++ config.php
@@ -1,1 +1,8 @@
-
+    define("db_host", "");
+		define("db_user", "");
+		define("db_pass", "");
+		define("db_name", "");
+		define("baseurl", "");
+		define("twitter_consumer_key", "");
+		define("twitter_consumer_secret", "");
+		define("path", dirname(BASEPATH));
