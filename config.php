945082decff57643a7493900c30366f2a0dba406
Muthukrishnan c <muthu.phpian@gmail.com>
2017-11-21 22:42:05 +0530

    Update config.php
    changes for new config

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
-		define("db_host", "localhost");
-		define("db_user", "root");
+		define("db_host", "");
+		define("db_user", "");
 		define("db_pass", "");
-		define("db_name", "test_data");
-		define("baseurl", "http://localhost/twitter/automation/reply_bot/index.php/");
-		define("twitter_consumer_key", "NXO9Z7GPGkzGVDMBEy5mejDlt");
-		define("twitter_consumer_secret", "oJhKxmywtCcNHNanzWDnZEJ4G1Y4WSUbsiAfdXMj1ABeT8dcHQ");
+		define("db_name", "");
+		define("baseurl", "");
+		define("twitter_consumer_key", "");
+		define("twitter_consumer_secret", "");
 		define("path", dirname(BASEPATH));
 
 
